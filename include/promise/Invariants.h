9bba429da98455889fbb46d1d2b18b74443b5b85
Jiahui Xu <jxu@ethz.ch>
2025-07-08 12:42:59 +0200

    Invariants: Using AST to for arbitrary formula

--- include/promise/Invariants.h
+++ include/promise/Invariants.h
@@ -6,8 +6,100 @@
 #include "kernel/yosys_common.h"
 
 #include <Eigen/Dense>
+#include <utility>
 
 USING_YOSYS_NAMESPACE
+
+// Forward declaration
+struct UnaryOp;
+struct BinaryOp;
+
+/// \brief: A type union for representing an arbitrary formula.
+///
+/// \example: "((3 * (a * b)) + (a -> c) <= 0)"
+using PropAst = std::variant<int, Yosys::RTLIL::IdString, UnaryOp, BinaryOp>;
+
+/// \brief: Syntax sugar for std::make_shared<PropAst>(...)
+inline std::shared_ptr<PropAst> mkAst(const PropAst &n) {
+  return std::make_shared<PropAst>(n);
+}
+
+/// \brief: Represent unary operators
+///
+/// \example: "!" (logical not), "~" (word-level negation), "AG" (an unary
+/// temporal operator).
+struct UnaryOp {
+  enum OpType {
+    LNOT,
+  };
+  OpType op;
+  std::shared_ptr<PropAst> arg;
+  UnaryOp(OpType op, std::shared_ptr<PropAst> arg)
+      : op(op), arg(std::move(arg)){};
+};
+
+/// \brief: Represent binary operators
+///
+/// \example: "+" (ADD), "<=" (LE)
+struct BinaryOp {
+  enum OpType {
+    ADD,
+    MUL,
+    IMPLIES,
+    LE,
+    EQ,
+  };
+  OpType op;
+  std::shared_ptr<PropAst> lhs;
+  std::shared_ptr<PropAst> rhs;
+
+  BinaryOp(OpType op, std::shared_ptr<PropAst> lhs,
+           std::shared_ptr<PropAst> rhs)
+      : op(op), lhs(std::move(lhs)), rhs(std::move(rhs)){};
+};
+
+/// A wrapper struct for dumping the AST as a string
+struct PrintVisitor {
+
+  std::string operator()(const int &n) const { return std::to_string(n); }
+  std::string operator()(const RTLIL::IdString &n) const { return log_id(n); }
+
+  std::string operator()(const BinaryOp &n) const {
+    const std::map<BinaryOp::OpType, string> tyToStr = {
+        {BinaryOp::ADD, "+"}, {BinaryOp::MUL, "*"}, {BinaryOp::IMPLIES, "->"},
+        {BinaryOp::LE, "<="}, {BinaryOp::EQ, "=="},
+    };
+    assert(tyToStr.count(n.op) > 0 && "Unknown binary operator!");
+    std::stringstream ss;
+    ss << "(" << visit(n.lhs) << " " << tyToStr.at(n.op) << " " << visit(n.rhs)
+       << ")";
+    return ss.str();
+  }
+
+  std::string operator()(const UnaryOp &n) const {
+    const std::map<UnaryOp::OpType, string> tyToStr = {
+        {UnaryOp::LNOT, "!"},
+    };
+    assert(tyToStr.count(n.op) > 0 && "Unknown unary operator!");
+    return tyToStr.at(n.op) + " " + visit(n.arg);
+  }
+
+  std::string visit(const std::shared_ptr<PropAst> &n) const {
+    // Remark for future visitors: this method complains if not all variant was
+    // implemented.
+    return std::visit(*this, *n);
+  }
+};
+
+/// \brief: A container class for adding various sanity checking on the AST
+struct Invariant {
+  std::shared_ptr<PropAst> ast;
+  Invariant(std::shared_ptr<PropAst> ast) : ast(std::move(ast)){};
+  std::string toString() const {
+    PrintVisitor visitor;
+    return visitor.visit(ast);
+  }
+};
 
 /// \brief A LinearInvariant represents a linear equality/inequality.  In
 /// general, a linear invariant can be:
